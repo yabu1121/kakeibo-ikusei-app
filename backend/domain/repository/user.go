863cb7d2f5d71034ea6e157cd4797223e7df4f5b
yabu1121 <n.hayato0122@outlook.jp>
2026-04-03 09:12:54 +0900

    login

--- backend/domain/repository/user.go
+++ backend/domain/repository/user.go
@@ -5,5 +5,4 @@ import "github.com/kakebon/backend/domain/model"
 type UserRepository interface {
 	Create(user *model.User) error
 	GetByEmail (email string) (*model.User, error)
-	Login(email string, password string) (*model.User, error)
 }
